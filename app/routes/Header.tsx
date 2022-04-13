efe531035a9452963ca83aa07505ca7fd470c939
Fanny Reslow <reslowf@gmail.com>
2022-04-13 12:16:44 +0200

    toggel navbarimg

---
+++ app/routes/Header.tsx
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar";
+export default function Header() {
+  return (
+    <section>
+      <Navbar />
+      <h1>Flashcard</h1>
+    </section>
+  );
+}
---
+++ app/routes/Navbar.tsx
@@ -0,0 +1,23 @@
+import closeIcon from "../images/close.png";
+import openIcon from "../images/open.png";
+
+import { useState } from "react";
+export default function Nav() {
+  // burger menu that drops down on mobile size
+  const [srcImg, setSrcImg] = useState(true);
+  function toggleNavImg() {
+    setSrcImg(!srcImg);
+  }
+
+  return (
+    <section>
+      <img
+        src={srcImg ? openIcon : closeIcon}
+        alt="burger-open"
+        width="20px"
+        height="20px"
+        onClick={toggleNavImg}
+      />
+    </section>
+  );
+}
